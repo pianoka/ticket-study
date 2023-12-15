ac6482c958c00b193bcdbf6e521099fee60708f0
pianoka <109780117+pianoka@users.noreply.github.com>
2023-12-16 05:01:33 +0900

    Add files via upload

---
+++ src/main/java/com/study/ticket/JwtTokenFilter.java
@@ -0,0 +1,58 @@
+package com.study.ticket;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter {
+    
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        // Header의 Authorization 값이 비어있으면 => Jwt Token을 전송하지 않음 => 로그인 하지 않음
+        if(authorizationHeader == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Header의 Authorization 값이 'Bearer '로 시작하지 않으면 => 잘못된 토큰
+        if(!authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 전송받은 값에서 'Bearer ' 뒷부분(Jwt Token) 추출
+        String token = authorizationHeader.split(" ")[1];
+
+        // 전송받은 Jwt Token이 만료되었으면 => 다음 필터 진행(인증 X)
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Jwt Token에서 loginId 추출
+        String loginId = JwtTokenUtil.getLoginId(token, secretKey);
+
+        // 추출한 loginId로 User 찾아오기
+        User loginUser = userService.getLoginUserByLoginId(loginId);
+
+        // loginUser 정보로 UsernamePasswordAuthenticationToken 발급
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginUser.getLoginId(), null, List.of(new SimpleGrantedAuthority(loginUser.getRole().name())));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // 권한 부여
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+
+}
